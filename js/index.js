5c032080b615a9b34d3d42b17b8f65caa4764918
Fahim-Hassan-Ovi <fahim.hassan.ovi@gmail.com>
2024-12-10 20:50:39 +0600

    Added the indexfile and also add the functionality of savings and calculate buttton

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// globally declare all the value
+// getting all the value
+
+
+// add event listener for calculate button
+const calculateButton = document.getElementById('calculate');
+calculateButton.addEventListener('click', function () {
+   const income = parseFloat(document.getElementById('income').value);
+   const software = parseFloat(document.getElementById('software').value);
+   const courses = parseFloat(document.getElementById('courses').value);
+   const internet = parseFloat(document.getElementById('internet').value);
+
+   // console.log({ income, software, courses, internet });
+   // console.table({ income, software, courses, internet });
+
+   const totalExpenses = software + courses + internet;
+   const balance = income - totalExpenses;
+   //    console.table({totalExpenses, balance});
+
+   const totalExpensesElement = document.getElementById('total-expenses');
+   totalExpensesElement.innerHTML = totalExpenses.toFixed(2);
+   const balanceElement = document.getElementById('balance');
+   balanceElement.innerHTML = balance.toFixed(2);
+
+   const result = document.getElementById('results');
+   result.classList.remove('hidden');
+});
+
+// add event listener for saving button
+const calculateSavingsButton = document.getElementById('calculate-savings');
+calculateSavingsButton.addEventListener('click', function () {
+   const savingPercentage = parseFloat(document.getElementById('savings').value);
+   const income = parseFloat(document.getElementById('income').value);
+   const software = parseFloat(document.getElementById('software').value);
+   const courses = parseFloat(document.getElementById('courses').value);
+   const internet = parseFloat(document.getElementById('internet').value);
+
+   // console.log(savingPercentage);
+
+   const totalExpenses = software + courses + internet;
+   const balance = income - totalExpenses;
+   const savingAmount = (savingPercentage * balance) / 100;
+   const remainingBalance = balance - savingAmount;
+   const savingElement = document.getElementById('savings-amount');
+   savingElement.innerText = savingAmount.toFixed(2);
+   // console.log(savingAmount);
+   const remainingElement = document.getElementById('remaining-balance');
+   remainingElement.innerText = remainingBalance.toFixed(2);
+   // console.log(savingAmount);
+})
